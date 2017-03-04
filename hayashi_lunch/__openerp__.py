547f1344ab6b81828be478af63487d3a8a6a6d49
toyokiki <itiki@toyo-kiki.co.jp>
2017-03-04 14:35:17 +0000

    add hayashi lunch

---
+++ hayashi_lunch/__openerp__.py
@@ -0,0 +1,20 @@
+
+# -*- coding: utf-8 -*-
+
+
+{
+        "name": "Hayashi no Tabelog",
+        "summary": "mainichi tabelog kakuyo",
+        "version": "9.0.0.1.1",
+        "category": "Uncategorized",
+        "website": "http://www.toyo-kiki.co.jp",
+        "author": "ichi2",
+        "license": "AGPL-3",
+#        "application": False,
+        "installable": True,
+        "depends": ["base",
+        ],
+        "data": [
+                "views/lunch_view.xml",
+        ],
+}
