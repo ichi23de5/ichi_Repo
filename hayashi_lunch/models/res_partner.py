028ca12d9d05fcd1987ad354afce64a07c7dadc4
ichi23de5 <ichi2266@nike.eonet.ne.jp>
2017-03-11 05:40:34 +0000

    hayashi_lunch deleted

--- hayashi_lunch/models/res_partner.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from openerp import fields, models
-
-class ResPartner(models.Model):
-    _inherit = 'res.partner'
-
-    relation_id  = fields.Many2one('hayashi', string='hayashi_lunch')
