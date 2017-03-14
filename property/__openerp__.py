f4bcfab667d20db393207caa3553414690b2c99b
toyokiki <itiki@toyo-kiki.co.jp>
2017-03-14 01:49:59 +0000

    add property

--- property/__openerp__.py
+++ property/__openerp__.py
@@ -4,7 +4,7 @@
     'summary': "Manage Properties of TOKI Constractions ",
     'author': "ichi2",
     'website': "http://www.toyo-kiki.co.jp",
-
+    'description': "Manage Properties of TKCLOUD or Constructions",
     # Categories can be used to filter modules in modules listing
     'installable': True,
     'category': "Property",
