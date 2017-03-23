996421c362569f04431fe9fdb2efc010e18c3c73
toyokiki <itiki@toyo-kiki.co.jp>
2017-03-23 00:29:29 +0000

    add warranty

---
+++ warranty/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "warranty_management",
+    'summary': "this module is for warranty management",
+    'author': "ichi2",
+    'website': "http://www.toyo-kiki.co.jp",
+
+    # Categories can be used to filter modules in modules listing
+    'installable': True,
+    'category': "Uncategorized",
+    'version': "9.0.0.1.1",
+    'depends': ['base','sale','product'],
+    'data': [
+        "views/warranty_view.xml",
+    ],
+    
+#    'demo': [
+        #'demo.xml',
+#    ],
+}
