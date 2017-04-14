01616925b62e2d0aeb53ca9f44697d0717f5aa0e
toyokiki <itiki@toyo-kiki.co.jp>
2017-04-14 13:19:15 +0000

    update property newer version

--- property/__openerp__.py
+++ property/__openerp__.py
@@ -1,17 +1,19 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Property Management",
-    'summary': "Manage Properties of TOKI Constractions ",
+    'name': "Property",
     'author': "ichi2",
     'website': "http://www.toyo-kiki.co.jp",
     'description': "Manage Properties of TKCLOUD or Constructions",
     # Categories can be used to filter modules in modules listing
     'installable': True,
-    'category': "Property",
+    'category': "Uncategorized",
     'version': "9.0.0.1.1",
-    'depends': ['base','sale'],
+    'depends': ['base','sale','product'],
     'data': [
+        'security/user_groups.xml',
+        'security/ir.model.access.csv',
         "views/property_view.xml",
+        "views/inspection_view.xml",
     ],
     
 }
---
+++ property/models/inspection.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+from openerp import models, fields, api
+from datetime import datetime, timedelta, date
+from dateutil.relativedelta import relativedelta
+
+
+class Inspection(models.Model):
+    _name = 'property.inspection'
+    _order = 'date desc'
+    _inherit = ['mail.thread', 'ir.needaction_mixin']
+
+
+    property_id = fields.Many2one('property', string='Property ID', required=True, readonly=True)
+    ### inspection ###
+    date = fields.Date(string='Date', required=True,)
+    inspector_id = fields.Many2one('res.users', string='Inspector')
+    act_type = fields.Selection([
+        ('inspect', 'Tenken'),
+        ('routine_inspection', 'Teikitenken'),
+        ('change', 'Koukan'),
+        ('repair', 'Syuri'),
+        ('coordinate', 'Tyousei'),
+        ('others', 'Other'),],
+        string='Act type')
+    inspection_note = fields.Text(string='Note')
+    product_memo = fields.Text(string='product_memo', help='Koukan sita kiki wo kaitene')
+    ### request ###
+    request_id = fields.Many2one('property.inspection.request', string='Request')
+    request_date = fields.Date(string='request_date', related='request_id.date', readonly=True)
+    requester_name = fields.Char(string='requester_name', related='request_id.partner_id.name', readonly=True)
+    request_note = fields.Text(string='request_note', related='request_id.request_note', readonly=True)
+    responder_name = fields.Char(string='responder_name', related='request_id.user_id.name', readonly=True)
+    ### ###
+    state = fields.Selection([
+        ('ongoing', 'Taioutyu'),
+        ('arranging', 'Tehaityu'),
+        ('finishing', 'Kanryo'),],
+        string='state')
+
+
+
+class InspectionRequest(models.Model):
+    _name = 'property.inspection.request'
+    _order = 'date desc'
+
+
+    date = fields.Date(string='Date', required=True, copy=False,)
+    partner_id = fields.Many2one('res.partner', string='partner_id',)
+    request_note = fields.Text(string='request_note',)
+    user_id = fields.Many2one('res.users', string='user_id', required=True, help='hosyu no irai wo uketahitoy')
+
+    @api.model
+    def create(self, vals):
+        if vals.get('name', 'New') == 'New':
+            vals['name'] = self.env['ir.sequence'].next_by_code('inspection.request') or 'New'
+        result = super(InspectionRequest, self).create(vals)
+        return result
+
