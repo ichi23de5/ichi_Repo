ce299690eb7024286513f4cefc289b436a2cb752
toyokiki <itiki@toyo-kiki.co.jp>
2017-04-19 09:28:38 +0000

    berry start

--- account_add_hayashi/__openerp__.py
+++ account_add_hayashi/__openerp__.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
 
 {
-        "name": " Sale add hayashi",
-        "summary": "res_partner[fix]",
+        "name": " Account add hayashi",
+        "summary": "",
         "version": "9.0.0.1.1",
-        "category": "Sales",
+        "category": "Account",
         "website": "http://www.toyo-kiki.co.jp",
         "author": "ichi2",
         "license": "AGPL-3",
         "application": False,
         "installable": True,
         "depends": [
-                   "sale","property","base"
+                   "account","property","sale"
                    ],
         "data": [
-                "views/sale_view.xml",
-                "views/product_view.xml",
-                "views/res_partner_view.xml",
+                "views/account_invoice_view.xml",
+#                "views/product_view.xml",
+#                "views/res_partner_view.xml",
                 ],
 }
 
--- aton_report/report/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- encoding: utf-8 -*-
-
-# are these lines needed??
-import quotation
-import invoice
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
