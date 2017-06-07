27da67ef935b088bc969de49b1c64f878d182f2b
toyokiki <itiki@toyo-kiki.co.jp>
2017-06-07 02:06:28 +0000

    teikiatsu

--- toki_sale/__openerp__.py
+++ toki_sale/__openerp__.py
@@ -17,6 +17,6 @@
                 "views/sale_view.xml",
                 "views/product_view.xml",
                 "views/res_partner_view.xml",
-                "report/report.xml",
+                "report/construction_report.xml",
                 ],
 }
