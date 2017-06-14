65086c77554e74d6a087ecbe336246e53f48ce64
toyokiki <itiki@toyo-kiki.co.jp>
2017-06-14 07:54:34 +0000

    start account

--- toki_bank/models/res_bank.py
+++ toki_bank/models/res_bank.py
@@ -7,7 +7,7 @@ from openerp import api, fields ,models,  _
 class Bank(models.Model):  
     _inherit = 'res.bank' 
 
-    branch = fields.Char(string='Branch Name')
+    branch = fields.Char('Branch Name')
 
 
 
@@ -19,5 +19,14 @@ class ResPartnerBank(models.Model):
         ('ordinary', 'Ordinary Account'),
         ('current', 'Current Account')
         ], string='type')
-    branch = fields.Char(string='Branch Name', related='bank_id.branch', readonly=True, store=True)
+    branch = fields.Char('Branch Name', related='bank_id.branch', readonly=True, store=True)
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+
+#    payee_bank = fields.Many2one('res.partner.bank', 'Hurikomisaki')
+#                                 domain="[('partner_id.id', '=', '1')]",
+#                                 help='seikyu surutoki no TOKI furikomisaki bank')
 
