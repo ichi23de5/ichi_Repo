6c1ea4574bf5759bd52f7fb29f48757782a35520
toyokiki <itiki@toyo-kiki.co.jp>
2017-06-28 04:26:21 +0000

    add line success

--- toki_account/models/account.py
+++ toki_account/models/account.py
@@ -41,7 +41,7 @@ class AccountPropertyLine(models.Model):
     completion_date = fields.Date('Syunkoubi', readonly=True, index=True)
     price_total = fields.Integer('Amount', readonly=True)
     purchase_number = fields.Char('Purchase Number', readonly=True, index=True)
-    act_type = fields.Many2one('account.act.type', 'Act Type', help='Seikyusyo no KoujiSyubetsu wo kaku.', index=True)
+    type_id = fields.Char('Act Type', help='Seikyusyo no KoujiSyubetsu wo kaku.', index=True)
 
 
 
@@ -67,31 +67,37 @@ class SaleOrder(models.Model):
     completion_date = fields.Date('Syunkoubi Nohinbi', states={'draft': [('readonly', True)], 'sent': [('readonly', True)]})
 
     ### reflect account.invoice ###
-#    @api.multi
-#    def _prepare_invoice(self):
-#        self.ensure_one()
-#        journal_id = self.env['account.invoice'].default_get(['journal_id'])['journal_id']
-#        if not journal_id:
-#            raise UserError(_('Please define an accounting sale journal for this company.'))
-#        invoice_vals = {
-#            'name': self.client_order_ref or '',
-#            'origin': self.name,
-#            'type': 'out_invoice',
-#            'account_id': self.partner_invoice_id.property_account_receivable_id.id,
-#            'partner_id': self.partner_invoice_id.id,
-#            'partner_shipping_id': self.partner_shipping_id.id,
-#            'journal_id': journal_id,
-#            'currency_id': self.pricelist_id.currency_id.id,
-#            'comment': self.note,
-#            'payment_term_id': self.payment_term_id.id,
-#            'fiscal_position_id': self.fiscal_position_id.id or self.partner_invoice_id.property_account_position_id.id,
-#            'company_id': self.company_id.id,
-#            'user_id': self.user_id and self.user_id.id,
-#            'team_id': self.team_id.id,
-#            'property_name': self.property_id.name or '',
-#            'completion_date': self.completion_date
-#        }
-#        return invoice_vals
+    @api.multi
+    def _prepare_invoice(self):
+        self.ensure_one()
+        journal_id = self.env['account.invoice'].default_get(['journal_id'])['journal_id']
+        if not journal_id:
+            raise UserError(_('Please define an accounting sale journal for this company.'))
+        invoice_vals = {
+            'name': self.client_order_ref or '',
+            'origin': self.name,
+            'type': 'out_invoice',
+            'account_id': self.partner_invoice_id.property_account_receivable_id.id,
+            'partner_id': self.partner_invoice_id.id,
+            'partner_shipping_id': self.partner_shipping_id.id,
+            'journal_id': journal_id,
+            'currency_id': self.pricelist_id.currency_id.id,
+            'comment': self.note,
+            'payment_term_id': self.payment_term_id.id,
+            'fiscal_position_id': self.fiscal_position_id.id or self.partner_invoice_id.property_account_position_id.id,
+            'company_id': self.company_id.id,
+            'user_id': self.user_id and self.user_id.id,
+            'team_id': self.team_id.id,
+            'property_line_ids': [(0, 0, {
+                'origin': self.name,
+                'property_id': self.property_id.id or False,
+                'completion_date': self.completion_date,
+                'price_total': self.amount_untaxed,
+                'purchase_number': self.purchase_number or False,
+                'type_id': self.act_type.type_id,
+            })],           
+        }
+        return invoice_vals
 
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
