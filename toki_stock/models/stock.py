00d214984e12f93a55e72ad2aca65e975f5f24f2
ichi23de5 <ichi2266@nike.eonet.ne.jp>
2017-07-13 06:05:20 +0000

    big

--- toki_stock/models/stock.py
+++ toki_stock/models/stock.py
@@ -6,14 +6,18 @@ from openerp import api, fields ,models,  _
 class stock_picking(models.Model): 
     _inherit = 'stock.picking'
 
-    property_name = fields.Char('Property Name', related='order_id.property_id.name')
+    property_name = fields.Char('Property Name')
     order_id = fields.Many2one('sale.order', 'Order Number')
 
     @api.onchange('order_id')
     def _onchange_order_number(self):
         sale = self.order_id.name
+        pro = self.order_id.property_id.name
+#        partner = self.order_id.partner_id
         if sale:
             self.update({'origin': sale})
+            self.update({'property_name': pro})
+#            self.update({'partner_id': partner})
             return
 
 
