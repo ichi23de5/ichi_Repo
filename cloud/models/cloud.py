dbede000bd2422d70efb57f7ab7ad38fed0ec0d2
ichi23de5 <ichi2266@nike.eonet.ne.jp>
2017-07-20 07:12:53 +0000

    cloud m

--- cloud/models/cloud.py
+++ cloud/models/cloud.py
@@ -4,47 +4,51 @@ from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
 
 
-class Cloud(models.Model):
-    _name = 'cloud'
+class CloudOrder(models.Model):
+    _name = 'cloud.order'
     _order = 'date_cloud desc'
     _inherit = ['mail.thread', 'ir.needaction_mixin']
 
 
     state = fields.Selection([ 
+         ('order', 'Order'),
          ('check', 'Check'), 
          ('work', 'Working'), 
          ('stop', 'Stopping'), 
          ('finish', 'Finished'), 
-         ], string='Status', readonly=True, copy=False, index=True, track_visibility='onchange', default='check', help='finish ni ittara saikeiyaku') 
-    date_cloud = fields.Date('Check Date', copy=False, default=fields.Datetime.now, required=True, index=True, readonly=True, states={'check': [('readonly', False)]})
-    order_id = fields.Many2one('sale.order', string='Quotation Number', required=True, domain=[('purchase_order','=',True),('check_state','=',('manager','president'))], help='Hachusyo naito check dekinai')
+         ], string='Status', readonly=True, copy=False, index=True, track_visibility='onchange', default='order',
+         help='Order: Eigyo kara Tantou he touroku order. Check: Tantou ga check to sotsu test. Working: Kadochu. Stopping: Teishichu. Finished: Keiyakukikan End.Teishizumi.'
+         ) 
+    date_cloud = fields.Date('Check Date', copy=False, default=fields.Datetime.now, required=True, index=True, readonly=True, states={'order': [('readonly', False)]})
+    communication_date = fields.Datetime('Communication Date', help='Sotsu kakunin Datetime order.')
+    order_id = fields.Many2one('sale.order', 'Quotation Number', required=True, domain=[('purchase_order','=',True),('check_state','=',('manager','president'))], help='Hachusyo naito check dekinai')
     #### auto input ### with 'order_id' ###
-    property_name = fields.Char(related='order_id.property_id.name', string='Property Name', store=True)
+    property_name = fields.Char(related='order_id.property_id.name', string='Property Name', store=True, help='Property name ha jiyu ni henkou siteyoi.')
     property_add = fields.Char(related='order_id.property_id.address', string='Property Add', readonly=True, store=True)
-    partner_id = fields.Char(related='order_id.partner_id.name', string='PartnerName', readonly=True, store=True)
-    user_id = fields.Char('Salesperson', related='order_id.user_id.name', readonly=True, store=True)
+    partner_id = fields.Many2one('res.partner', related='order_id.partner_id', string='PartnerName', readonly=True)
+    user_id = fields.Many2one('res.users', 'Salesperson', related='order_id.user_id', readonly=True)
     plan = fields.Selection([
            ('lite', 'Lite'),
            ('basic', 'Basic'),
            ('entry', 'Entry')],         
            string='TKCLOUD Plan', required=True)
     rate_id = fields.Many2one('cloud.rate.plan', 'Rate Plan', required=True)
-#    rate_plan = fields.Selection([
-#                ('monthly', 'Monthly Amount'),
-#                ('yearly', 'Yearly Amount'),
-#                ('lump2', 'Lump sum 2'),
-#                ('lump5', 'Lump sum 5'),
-#                ('lump7', 'Lump sum 7'),
-#                ('incurred', 'Incurred Amount')],
-#                string='TKCLOUD Rate Plan', required=True)
     end_user = fields.Char('User Name')
     end_phone = fields.Char('User Phone Number')
     contact_check = fields.Boolean('Contact OK')
     dvr1 = fields.Many2one('product.product', string='dvr1')
+    cam1 = fields.Integer('cam1')
+    hdd1 = fields.Integer('hdd1')
     dvr2 = fields.Many2one('product.product', string='dvr2')
+    cam2 = fields.Integer('cam2')
+    hdd2 = fields.Integer('hdd2')
     dvr3 = fields.Many2one('product.product', string='dvr3')
+    cam3 = fields.Integer('cam3')
+    hdd3 = fields.Integer('hdd3')
     dvr4 = fields.Many2one('product.product', string='dvr4')
-    sim_id = fields.Many2one('sim', string='SIM User Number', required=True, copy=False)
+    cam4 = fields.Integer('cam4')
+    hdd4 = fields.Integer('hdd4')
+    sim_id = fields.Many2one('cloud.sim', string='SIM IP address', copy=False)
     memo = fields.Text('Memo')
 
     contractor_number = fields.Char('Contractor ID')
@@ -52,9 +56,9 @@ class Cloud(models.Model):
     application_date = fields.Date('Apprication Date')
     approval_date = fields.Date('Approval Date')
 
-    construction_date = fields.Date('Construction Date', readonly=True, states={'check': [('readonly', False)]})
+    construction_date = fields.Date('Construction Date')
     #### auto input ### with constraction_date ###
-    expiration_date = fields.Date('Expiration Date', store=True, readonly=True, states={'check': [('readonly', False)]})
+    expiration_date = fields.Date('Expiration Date', store=True)
     changeapp_date = fields.Date('Changeapp Date')
     cancelapp_date = fields.Date('Cancelapp Date')
     cancel_date = fields.Date('Cancel Date')
@@ -71,6 +75,10 @@ class Cloud(models.Model):
 
 
     ### state shift button ###
+    @api.multi
+    def check(self):
+        self.write({'state': 'check'})
+
     @api.multi 
     def start(self): 
         self.write({'state': 'work'}) 
@@ -104,4 +112,3 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template' 
 
     rate_id = fields.Many2one('cloud.rate.plan', 'CLOUD Rate Plan')
-
--- cloud/models/sim.py
+++ cloud/models/sim.py
@@ -4,16 +4,16 @@ from datetime import datetime, timedelta, date
 from dateutil.relativedelta import relativedelta
 
 
-class Sim(models.Model):
-    _name = 'sim'
+class CloudSim(models.Model):
+    _name = 'cloud.sim'
     _order = 'date_sim desc'
     _inherit = ['mail.thread', 'ir.needaction_mixin']
-    _rec_name = 'user_number'
+    _rec_name = 'ip_address'
 
     user_number = fields.Char(string='SIM User Number', required=True, copy=False,)
     phone = fields.Char(string='SIM Tel Number', required=False, copy=False,)
     sim_id = fields.Many2one('sim.type', string='SIM Type ID')
-    date_sim = fields.Datetime(string='Record Date', required=True, index=True, copy=False, default=fields.Datetime.now, help='SIM moushikonda date')
+    date_sim = fields.Datetime(string='Record Date', required=True, copy=False, default=fields.Datetime.now, help='SIM moushikonda date')
     iccid_number = fields.Char(string='Iccid Number', copy=False,)
 
     reception_date = fields.Date(string='Reception Date', required=True, copy=False, store=True, index=True,)
@@ -22,7 +22,7 @@ class Sim(models.Model):
     charge_date = fields.Date(string='Charge Date', store=True)
     min_month = fields.Date(string='Minimum Usage Date', store=True)
     expiration_date = fields.Date(string='Expiration Date', store=True)
-    ip_address = fields.Char('IP Address')
+    ip_address = fields.Char('IP Address', copy=False, required=True)
 
 
     @api.onchange('reception_date')
@@ -42,11 +42,11 @@ class Sim(models.Model):
 
 
 
-class SimType(models.Model):
-    _name = 'sim.type'
-    _rec_name = 'sim_type_id' 
+class CloudSimType(models.Model):
+    _name = 'cloud.sim.type'
+    _rec_name = 'name' 
 
-    sim_type_id = fields.Char(string='SIM Type ID', required=True, copy=False,)
+    name = fields.Char(string='SIM Type ID', required=True, copy=False,)
     max_storage = fields.Char(string='Max Strage')
     business_number = fields.Char(string='Business Number')
     size_code = fields.Char(string='Size Code')
