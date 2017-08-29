90b342caf5e4638718b346e83dbf9bcf3abe3067
ichi23de5 <ichi2266@nike.eonet.ne.jp>
2017-08-29 05:16:55 +0000

    backup0829

---
+++ toki_report/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+        "name": " TOKI report",
+        "summary": "construction ",
+        "version": "9.0.0.1.1",
+        "category": "Sales",
+        "website": "http://www.toyo-kiki.co.jp",
+        "author": "ichi2",
+        "license": "AGPL-3",
+        "application": False,
+        "installable": True,
+        "depends": [
+                   "sale","property","base","account","toki_sale",
+                   ],
+        "data": [
+                "views/sale_report.xml", 
+#                "report/sale_construction_template.xml",
+                ],
+}
