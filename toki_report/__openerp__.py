2dcf4bf6fa26fe2c4c5b07d8bfc559fdfcead9f3
ichi23de5 <ichi2266@nike.eonet.ne.jp>
2017-09-05 07:02:03 +0000

    account report

--- toki_report/__openerp__.py
+++ toki_report/__openerp__.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 {
         "name": " TOKI report",
         "summary": "construction ",
@@ -11,16 +10,18 @@
         "application": False,
         "installable": True,
         "depends": [
-                   "sale","property","base","account","purchase","report","toki_sale","sale_layout",
+                   "sale","property","base","account","purchase","report","toki_sale","toki_account","toki_bank","sale_layout",
                    ],
         "data": [
-                "security/ir.model.access.csv",
-                "views/custom_report.xml", 
+                "security/ir.model.access.csv", 
                 "views/sale_view.xml",
                 "views/res_partner_view.xml",
                 "report/sale_order_template.xml",
                 "report/sale_construction_template.xml",
                 "report/outside_order_template.xml",
+                "report/account_invoice_header.xml",
+                "report/account_invoice_template.xml",
                 "report/sale_layout_add.xml",
+                "views/custom_report.xml",
                 ],
 }
