dae57474f30bcacc384f843451892811f07d4d98
ichi23de5 <itiki@toyo-kiki.co.jp>
2017-09-22 10:27:56 +0000

    build sale_partner_change work OK

---
+++ sale_partner_change/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+{
+        "name": " TOKI Sale Partner Change",
+        "summary": "If partner_id changed then pricelist change",
+        "version": "9.0.0.1.1",
+        "category": "Sales",
+        "website": "http://www.toyo-kiki.co.jp",
+        "author": "ichi2",
+        "license": "AGPL-3",
+        "application": False,
+        "installable": True,
+        "depends": [
+                   "sale",
+                   ],
+        "data": [
+#                "views/sale_view.xml",
+#                "views/product_view.xml",
+#                "views/res_partner_view.xml",
+#                "report/construction_report.xml",
+                ],
+}
---
+++ sale_partner_change/models/sale.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+from openerp import api, fields ,models,  _
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+
+
+    @api.multi
+    @api.onchange('partner_id')
+    def onchange_partner_id(self):
+        """
+        Update the following fields when the partner is changed:
+        - Pricelist
+        - Payment term
+        - Invoice address
+        - Delivery address
+        """
+        if not self.partner_id:
+            self.update({
+                'partner_invoice_id': False,
+                'partner_shipping_id': False,
+                'payment_term_id': False,
+                'fiscal_position_id': False,
+            })
+            return
+
+        addr = self.partner_id.address_get(['delivery', 'invoice'])
+        values = {
+            'pricelist_id': self.partner_id.property_product_pricelist and self.partner_id.property_product_pricelist.id or False,
+            'payment_term_id': self.partner_id.property_payment_term_id and self.partner_id.property_payment_term_id.id or False,
+            'partner_invoice_id': addr['invoice'],
+            'partner_shipping_id': addr['delivery'],
+        }
+        if self.env.user.company_id.sale_note:
+            values['note'] = self.with_context(lang=self.partner_id.lang).env.user.company_id.sale_note
+
+        if self.partner_id.user_id:
+            values['user_id'] = self.partner_id.user_id.id
+        if self.partner_id.team_id:
+            values['team_id'] = self.partner_id.team_id.id
+        self.update(values)
+
+        for line in self.order_line:
+        
+            if line.order_id.pricelist_id and line.order_id.partner_id:
+                product = line.product_id.with_context(
+                    lang=line.order_id.partner_id.lang,
+                    partner=line.order_id.partner_id.id,
+                    quantity=line.product_uom_qty,
+                    date=line.order_id.date_order,
+                    pricelist=line.order_id.pricelist_id.id,
+                    uom=line.product_uom.id,
+                    fiscal_position=line.env.context.get('fiscal_position')
+                )
+#                line.price_unit = self.env['account.tax']._fix_tax_included_price(product.price, product.taxes_id, self.tax_id)
+
+
+            product_price = self.env['account.tax']._fix_tax_included_price(product.price, product.taxes_id, line.tax_id)
+            line.update({'price_unit': product_price})
+            price = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
+            taxes = line.tax_id.compute_all(price, line.order_id.currency_id, line.product_uom_qty, product=line.product_id, partner=line.order_id.partner_shipping_id)
+            line.update({
+                'price_tax': taxes['total_included'] - taxes['total_excluded'],
+                'price_total': taxes['total_included'],
+                'price_subtotal': taxes['total_excluded'],
+            })
+
