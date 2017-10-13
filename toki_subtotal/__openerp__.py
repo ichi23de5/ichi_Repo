e2dbf711dafc992b0e5fe19cfc25ba9ef32a1fbf
ichi23de5 <itiki@toyo-kiki.co.jp>
2017-10-13 03:25:58 +0000

    Build toki_subtotal

---
+++ toki_subtotal/__openerp__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+
+{
+    'name': 'Show Subtotal in SaleOrder ',
+    'author': 'ichi2',
+    'category': 'Sales Management',
+    'summary': 'Add Subtotal per sale layout category in SaleOrder',
+    'website': 'http://www.toki.com',
+    'version': '9.0.1.0.0',
+    'sequence': 1,
+    'depends': ['sale_layout', 'sale'],
+    'data': ['views/sale_view.xml'],
+    'installable': True,
+}
