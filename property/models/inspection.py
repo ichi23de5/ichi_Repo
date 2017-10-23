d9b1ff4848e9dd1e718c88aa4d615c0e98704ac8
ichi23de5 <ichi2266@nike.eonet.ne.jp>
2017-10-23 06:58:12 +0000

    delete inspection.request

--- property/models/inspection.py
+++ property/models/inspection.py
@@ -25,11 +25,14 @@ class Inspection(models.Model):
     inspection_note = fields.Text('Note')
     product_memo = fields.Text('product_memo', help='Koukan sita kiki wo kaitene')
     ### request ###
-    request_id = fields.Many2one('property.inspection.request', 'Request')
-    requester_name = fields.Char('requester_name', related='request_id.partner_id.name', readonly=True)
-    requester_rep_name = fields.Char('requester_name', related='request_id.rep_partner_id.name', readonly=True)
-    request_note = fields.Text('request_note', related='request_id.request_note', readonly=True)
-    responder_name = fields.Char('responder_name', related='request_id.user_id.name', readonly=True)
+    date = fields.Date('Date', required=True, copy=False,)
+    requester_id = fields.Many2one('res.partner', 'partner_id', domain="[('customer','=',True), ('company_type','=','company')]")
+    requester_rep_id = fields.Many2one('res.partner', 'Rep Name', domain="[('company_type','=','person'), ('parent_id', '=', requester_id)]")
+    request_note = fields.Text('request_note',)
+    responder_id = fields.Many2one('res.users', 'user_id', required=True, help='hosyu no irai wo uketahitoy')
+
+
+
  
 #    state = fields.Selection([
 #        ('ongoing', 'Taioutyu'),
@@ -42,22 +45,4 @@ class Inspection(models.Model):
     ### TKCLOUD Mail Notification ###
     symptom = fields.Char('Symptom', help='Tsuchi shita mail no syojo wo sonomama kaku. Shinkihassei no tokidake touroku suru.')
     mail_comment = fields.Char('Remarks', help='Tsuchi shita mail no Bikou wo sonomama kaku.')   
-
-class InspectionRequest(models.Model):
-    _name = 'property.inspection.request'
-    _order = 'date desc'
-    _rec_name = 'date'
-
-    date = fields.Date('Date', required=True, copy=False,)
-    partner_id = fields.Many2one('res.partner', 'partner_id', required=True, domain="[('customer','=',True), ('company_type','=','company')]")
-    rep_partner_id = fields.Many2one('res.partner', 'Rep Name', required=True, domain="[('company_type','=','person'), ('parent_id', '=', partner_id)]")
-    request_note = fields.Text('request_note',)
-    user_id = fields.Many2one('res.users', 'user_id', required=True, help='hosyu no irai wo uketahitoy')
-
-    @api.model
-    def create(self, vals):
-        if vals.get('name', 'New') == 'New':
-            vals['name'] = self.env['ir.sequence'].next_by_code('inspection.request') or 'New'
-        result = super(InspectionRequest, self).create(vals)
-        return result
 
