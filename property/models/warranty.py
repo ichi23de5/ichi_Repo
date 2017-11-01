8dc48009093af00ed0b66f26508fcabac680f7be
ichi23de5 <ichi2266@nike.eonet.ne.jp>
2017-11-01 00:26:04 +0000

    backup

--- property/models/warranty.py
+++ property/models/warranty.py
@@ -11,9 +11,10 @@ class ProductWarranty(models.Model):
     warranty_id = fields.Many2one('product.template', 'Name', required=True, domain="[('is_warranty','=',True)]")
     start_date = fields.Date('Start Date')
     end_date = fields.Date('End Date')
-    property_war_id = fields.Many2one('property', 'Property warranty ID', index=True, ondelete='cascade', oldname='property_id')
+    action = fields.Boolean('Action')
+    property_war_id = fields.Many2one('property', 'Property warranty ID', index=True, ondelete='cascade', oldname='property_id', readonly=True)
 
-#    warranty_sch_ids = fields.One2many('product.warranty.schedule','warranty_sch_id', 'Schedule')
+    warranty_sch_ids = fields.One2many('product.warranty.schedule','warranty_sch_id', 'Schedule')
 
     @api.onchange('start_date')
     def onchange_period(self):
@@ -30,12 +31,13 @@ class ProductWarrantySchedule(models.Model):
     _rec_name = 'warranty_id'
 
     warranty_id = fields.Many2one('product.template', 'Name', domain="[('is_warranty','=',True)]")
-    date = fields.Date('Date')
+    date = fields.Date('Date', help='Tenken iku meyasu no hi.')
     type = fields.Selection([
         ('teiki', 'Teiki tenken'),
         ('end', 'Warranty End')],
         'Type')
     finish = fields.Boolean('Finish')
+    property_id = fields.Many2one('property', 'Property Name', related='warranty_sch_id.property_war_id', readonly=True)
 
     warranty_sch_id = fields.Many2one('product.warranty', 'Property warranty ID', index=True, ondelete='cascade')
 
@@ -50,3 +52,28 @@ class Product(models.Model):
     scope_of_coverage = fields.Char('Scope of Coverage')
     range_coverage = fields.Integer('range_coverage')
     count_inspection = fields.Integer('Inspection Count')
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.multi
+    def test_button(self):
+        val_set = {}
+        if any(line.product_tmpl_id.is_warranty == 1 for line in self.order_line):
+            start = fields.Date.from_string(self.completion_date)
+            for line in self.order_line:
+                if line.product_tmpl_id.is_warranty == 1:
+                    period = line.product_id.range_coverage
+                    exp = start + relativedelta(years=period)
+                    self.env['product.warranty'].create({
+                        'warranty_id' : line.product_id.id,
+                        'range_coverage' : period,
+                        'property_war_id' : line.order_id.property_id.id,
+#                        'active' : True,
+                        'start_date' : line.order_id.completion_date,
+                        'end_date' : exp,
+                })
+        else:
+            raise UserError("Warranty item ga arimasen!!")
+        return
